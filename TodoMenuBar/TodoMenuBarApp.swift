73d5560feeebb4c94a00d71972c4ec731fab54ee
marc j. greenberg <codemarc@gmail.com>
2024-11-08 05:53:42 -0500

    Initial Commit

---
+++ TodoMenuBar/TodoMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMenuBarApp.swift
+//  TodoMenuBar
+//
+//  Created by Marc J. Greenberg on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoMenuBarTests/TodoMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMenuBarTests.swift
+//  TodoMenuBarTests
+//
+//  Created by Marc J. Greenberg on 11/8/24.
+//
+
+import Testing
+@testable import TodoMenuBar
+
+struct TodoMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
